b91e9385dfb5e29b4dc506330a762164d92c94eb
OEM <gurleva_ludmila@mail.ru>
2024-02-28 20:24:56 +0300

    Скриншоты

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -22,6 +22,7 @@ test.describe("Авторизация", () => {
     await page.getByPlaceholder('Пароль').fill(password);
     await page.getByTestId('login-submit-btn').click();
     await expect(page.getByTestId('header-top').getByRole('link', { name: 'Медиа Нетологии' })).toBeVisible();
+    await page.screenshot({ path: "screenshotSuccessful.png", fullPage: true });
   });
    
 
@@ -40,5 +41,6 @@ test.describe("Авторизация", () => {
     const textError = await page.$eval('[data-testid="login-error-hint"]', (element) => element.textContent);
     const expectedText = "Вы ввели неправильно логин или пароль";
     expect(textError).toBe(expectedText); 
+    await page.screenshot({ path: "screenshotFailed.png", fullPage: true });
   });
 });
