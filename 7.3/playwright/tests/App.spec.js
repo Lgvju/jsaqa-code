ccafce34436c2ed47f3c75665642bf69a300aeaa
OEM <gurleva_ludmila@mail.ru>
2024-02-28 22:52:00 +0300

    скриншоты

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -9,6 +9,25 @@ const {
  
 
 test.describe("Авторизация", () => {
+
+  test('Неуспешная авторизация', async ({ page }) => {
+    const browser = await chromium.launch({
+     headless: false,
+     slowMo: 500
+   });  
+   await page.goto('https://netology.ru/');
+   await page.getByRole('link', { name: 'Войти' }).click();
+   await page.getByPlaceholder('Email').click();
+   await page.getByPlaceholder('Email').fill(emailInvalid);
+   await page.getByPlaceholder('Пароль').fill(passwordInvalid);
+   await page.getByTestId('login-submit-btn').click();
+   await page.waitForSelector('[data-testid="login-error-hint"]', { timeout: 2000 });
+   const textError = await page.$eval('[data-testid="login-error-hint"]', (element) => element.textContent);
+   const expectedText = "Вы ввели неправильно логин или пароль";
+   expect(textError).toBe(expectedText); 
+   await page.screenshot({ path: "screenshotFailed.png", fullPage: true });
+  });
+
   test('Успешная авторизация', async ({ page }) => {
       const browser = await chromium.launch({
       headless: false,
@@ -21,26 +40,8 @@ test.describe("Авторизация", () => {
     await page.getByPlaceholder('Email').fill(email);
     await page.getByPlaceholder('Пароль').fill(password);
     await page.getByTestId('login-submit-btn').click();
-    await expect(page.getByTestId('header-top').getByRole('link', { name: 'Медиа Нетологии' })).toBeVisible();
+    //await expect(page.getByTestId('header-top').getByRole('link', { name: 'Медиа Нетологии' })).toBeVisible();  
+    await page.getByRole('link', { name: 'Расписание занятий' }).click();  
     await page.screenshot({ path: "screenshotSuccessful.png", fullPage: true });
   });
-   
-
-  test('Неуспешная авторизация', async ({ page }) => {
-     const browser = await chromium.launch({
-      headless: false,
-      slowMo: 500
-    });  
-    await page.goto('https://netology.ru/');
-    await page.getByRole('link', { name: 'Войти' }).click();
-    await page.getByPlaceholder('Email').click();
-    await page.getByPlaceholder('Email').fill(emailInvalid);
-    await page.getByPlaceholder('Пароль').fill(passwordInvalid);
-    await page.getByTestId('login-submit-btn').click();
-    await page.waitForSelector('[data-testid="login-error-hint"]', { timeout: 2000 });
-    const textError = await page.$eval('[data-testid="login-error-hint"]', (element) => element.textContent);
-    const expectedText = "Вы ввели неправильно логин или пароль";
-    expect(textError).toBe(expectedText); 
-    await page.screenshot({ path: "screenshotFailed.png", fullPage: true });
-  });
-});
+ });
