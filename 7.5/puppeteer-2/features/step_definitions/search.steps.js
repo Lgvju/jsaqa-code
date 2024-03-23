3eeccc793d1db6b1a2ecd70a53f34ff39b856ccf
OEM <gurleva_ludmila@mail.ru>
2024-03-23 23:11:05 +0300

    task 1

--- 7.5/puppeteer-2/features/step_definitions/search.steps.js
+++ 7.5/puppeteer-2/features/step_definitions/search.steps.js
@@ -1,8 +1,10 @@
 const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
-const { Given, When, Then, Before, After } = require("cucumber");
-const { putText, getText } = require("../../lib/commands.js");
+const { Given, When, Then, Before, After,setDefaultTimeout } = require('@cucumber/cucumber');
+const { clickElement, getText } = require("../../lib/commands.js");
+const {setDefaultTimeout} = require('@cucumber/cucumber');
+setDefaultTimeout(60 * 1000);
 
 Before(async function () {
   const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
@@ -18,17 +20,44 @@ After(async function () {
 });
 
 Given("user is on {string} page", async function (string) {
-  return await this.page.goto(`https://netology.ru${string}`, {
-    setTimeout: 20000,
+  return await this.page.goto(`${string}`, {
+      setTimeout: 60000,
   });
 });
-
-When("user search by {string}", async function (string) {
-  return await putText(this.page, "input", string);
+When("user choose day", async function () {
+ return await clickElement(this.page, "page-nav__day-number");
+},70000);
+When("user choose time", async function () {
+  await clickElement(this.page, "div.movie-seances__hall");
 });
-
-Then("user sees the course suggested {string}", async function (string) {
-  const actual = await getText(this.page, "a[data-name]");
-  const expected = await string;
-  expect(actual).contains(expected);
+When("user select 1 row 10 seat", async function () {
+  await clickElement(this.page, ".buying-scheme__chair_standart > span:nth-child(10)");
+},70000);
+When("user select 1 row 5 seat", async function () {
+  await clickElement(this.page, ".buying-scheme__chair_standart> span:nth-child(5)");
 });
+When("user select 1 row 6 seat", async function () {
+  await clickElement(this.page, ".buying-scheme__chair_standart> span:nth-child(6)");
+  });
+When("user select the booked place", async function () {
+  await clickElement(this.page, ".buying-scheme__chair_standart> span:nth-child(10)");
+});
+When("user click button", async function () {
+  await clickElement(this.page, "button.acceptin-button");
+  });
+When("user click receive QR", async function () {
+  await clickElement(this.page, "button.acceptin-button");
+});
+Then("user see text {string}", async function (string) {
+    const actual = await getText(this.page, "p.ticket__check");
+    const expected = await string;
+    expect(actual).contains(expected);
+  });
+Then("user see button disabled {string}", async function (string) {
+    const actual = String(await this.page.$eval("button", (button) => {
+        return button.disabled;
+      })
+    );
+    const expected = await string;
+    expect(actual).contains(expected);
+  });
---
+++ 7.5/puppeteer-2/tickets.test.js
@@ -0,0 +1,46 @@
+const { clickElement, getText } = require('./lib/commands.js');
+const puppeteer = require('puppeteer');
+
+let page;
+
+beforeEach(async () => {
+  page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(70000);
+  await page.goto('http://qamid.tmweb.ru/client/index.php');
+});
+
+afterEach(() => {
+	page.close();
+});
+
+describe('Ticket booking tests', () => {
+
+ test('Should successfully book one ticket', async () => {
+		await clickElement(page, '.page-nav > a:nth-child(5)');
+		await clickElement(page, 'a.movie-seances__time');
+		await clickElement(page, '.buying-scheme__row > span:nth-child(1)');
+		await clickElement(page, 'button.acceptin-button');
+		await clickElement(page, 'button.acceptin-button');
+		const actual = await getText(page, 'p.ticket__hint');
+		expect(actual).toContain(
+			'Покажите QR-код нашему контроллеру для подтверждения бронирования.'); 
+	});
+	test('Should successfully book two tickets', async () => {
+		await clickElement(page, '.page-nav > a:nth-child(5)');
+		await clickElement(page, 'a.movie-seances__time');
+		await clickElement(page, '.buying-scheme__row > span:nth-child(2)');
+		await clickElement(page, '.buying-scheme__row > span:nth-child(3)');
+		await clickElement(page, 'button.acceptin-button');
+		await clickElement(page, 'button.acceptin-button');
+		const actual = await getText(page, 'p.ticket__hint');
+		expect(actual).toContain(
+			'Покажите QR-код нашему контроллеру для подтверждения бронирования.'); 
+	});
+	test('Should unsuccessful to book already booked ticket', async () => {
+		await clickElement(page, '.page-nav > a:nth-child(5)');
+		await clickElement(page, 'a.movie-seances__time');
+		await clickElement(page, '.buying-scheme__row > span:nth-child(1)');
+		expect(String(await page.$eval("button", (button) => {
+		return button.disabled;}))).toContain("true"); 
+	});
+});
